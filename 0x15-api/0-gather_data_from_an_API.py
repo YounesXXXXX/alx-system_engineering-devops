b31c901633fd181ca0ecb4d84b09454022a9fda5
Younes Ait Lahsen <144718552+YounesXXXXX@users.noreply.github.com>
2024-05-21 05:15:42 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
+"""Returns info about employee's todo list progress."""
 
 import requests
 import sys
 
-if __name__ == "__main__":
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    userUrl = "https://jsonplaceholder.typicode.com/users/"
+    userInfo = userUrl + empId
+    taskInfo = userUrl + empId + "/todos"
 
-    name = user.json().get('name')
+    # Send API requests
+    userResponse = requests.get(userInfo)
+    taskResponse = requests.get(taskInfo)
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
-    completed = 0
+    # Get employee name from userResponse
+    empName = userResponse.json().get('name')
 
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
+    # Get tasks from taskResponse as todos
+    todos = taskResponse.json()
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
+    # Store number of todos in a variable allTasks
+    allTasks = len(todos)
 
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
+    # Create storage array for completed todos
+    compTasks = []
+
+    # Create counter for completed todos
+    comp = 0
+
+    for item in todos:
+        if item['completed'] is True:
+            compTasks.append(item)
+            comp += 1
+
+    print(f'Employee {empName} is done with tasks({comp}/{allTasks}):')
+    for item in compTasks:
+        print(f'\t {item["title"]}')
