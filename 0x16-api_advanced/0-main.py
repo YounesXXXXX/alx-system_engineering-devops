2d86f39b9b6ea8193d7071787cf911483e0288e7
Younes Ait Lahsen <144718552+YounesXXXXX@users.noreply.github.com>
2024-06-05 05:48:05 +0100

    Update 0-main.py

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-import sys
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return 0
